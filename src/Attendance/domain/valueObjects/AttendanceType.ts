8da9bcd0b562401bab05105a53a87526fe3216a4
sugiwa <sugi.warra@gmail.com>
2023-09-04 00:07:11 +0900

    fix add union type in attendance type

--- src/Attendance/domain/valueObjects/AttendanceType.ts
+++ src/Attendance/domain/valueObjects/AttendanceType.ts
@@ -3,9 +3,12 @@ import { ValueObject } from '@/shared/domain/ValueObject';
 export const ATTENDANCE_TYPE = {
   begin: 0,
   finish: 1,
-};
+} as const;
 
-export class AttendanceType extends ValueObject<0 | 1> {
+type AttendanceTypeKeys = keyof typeof ATTENDANCE_TYPE;
+type AttendanceTypeValues = (typeof ATTENDANCE_TYPE)[AttendanceTypeKeys];
+
+export class AttendanceType extends ValueObject<AttendanceTypeValues> {
   equals(other: ValueObject<number>): boolean {
     return this._value === other._value;
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeeModule } from './employee/employee.module';
 import { CompanyModule } from './company/company.module';
+import { AttendanceModule } from './Attendance/attendance.module';
 
 @Module({
-  imports: [EmployeeModule, CompanyModule],
+  imports: [EmployeeModule, CompanyModule, AttendanceModule],
   controllers: [],
   providers: [],
 })
