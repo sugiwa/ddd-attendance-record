e92a0ccadc7946e99cc8dacee3253819c991eeb6
sugiwa <sugi.warra@gmail.com>
2023-10-01 22:37:11 +0900

    feat sing up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,35 @@
 import { API_KEY, API_URL } from '@/constants/auth';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 import { AuthResponse, createClient } from '@supabase/supabase-js';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly configService: ConfigService) {}
+  private prisma: PrismaClient;
   private supabase = createClient(
     this.configService.get(API_URL),
     this.configService.get(API_KEY),
   );
 
+  constructor(private readonly configService: ConfigService) {
+    this.prisma = new PrismaClient();
+  }
+
   async signUp(email: string, password: string) {
-    const authResponse: AuthResponse = await this.supabase.auth.signUp({
-      email,
-      password,
+    const hashPass = await bcrypt.hash(password, 10);
+    const employeeId = -1;
+
+    const res = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashPass,
+        employeeId,
+      },
     });
-    const { data, error } = authResponse;
-    const { user } = data;
-    return { user, error };
+
+    return res;
   }
 
   async signIn(email: string, password: string) {
